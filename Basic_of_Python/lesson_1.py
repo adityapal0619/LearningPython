9f2543c75972af485d50fcaede4700a50dbd1066
Aditya Pal <adityadd2001@gmail.com>
2025-07-28 10:01:45 +0530

    updatedlesson

---
+++ Basic_of_Python/lesson_1.py
@@ -0,0 +1,20 @@
+'''
+Python is a powerful, easy-to-learn programming language.
+It has simple syntax, huge community support, and is beginner-friendly.
+'''
+'Your First Python Program'
+print("Hello, World!")
+'print() is a function that displays text to the screen.'
+''' Variables:-Used to store data'''
+name = "Aditya"
+age = 21
+name = input("What is your name? ")
+print("Hello " + name)
+age=input("What is your age? ")
+print(name+" your age is "+age)
+'''Python is dynamically typed (no need to declare type)
+Variable names are case-sensitive
+input() always returns a string
+Use int() or float() to convert if needed'''
+
+ 
