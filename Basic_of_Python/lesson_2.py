5132561388ddb3e212b798d7f00b8778a61e938c
Aditya Pal <adityadd2001@gmail.com>
2025-08-06 22:21:37 +0530

    updatedcode

---
+++ Basic_of_Python/lesson_2.py
@@ -0,0 +1,16 @@
+#Create 3 variables: name, age, and is_student. Print them using print().
+
+name="Aditya";
+age=12;
+is_student=True;
+
+print(name);
+print(age);
+print(is_student);
+
+#Take the user's name and age as input and print
+
+name=input("Enter the name:- ");
+age=int(input("Enter the age:- "));
+
+print(f"Hello {name}, you are {age} years old.");
